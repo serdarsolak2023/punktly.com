0e829a7d161f5c61c5cd42c80270dd8e3118332f
Solak-App <serdarsolak2023@gmail.com>
2026-05-11 13:09:42 +0200

    bubblewrap.json twa-manifest.json android/

---
+++ app/src/main/java/de/punktlycoinly/app/DelegationService.java
@@ -0,0 +1,18 @@
+package de.punktlycoinly.app;
+
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
