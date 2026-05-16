44f3b05817072007cb8010d1d50b4ce984adaa32
Solak-App <serdarsolak2023@gmail.com>
2026-05-16 19:30:28 +0200

    Update page layout and add math tasks

--- app/mathTasks.ts
+++ app/mathTasks.ts
@@ -1,21 +1,11 @@
 export const mathTasks = [
   {
     category: "leicht",
-    minAge: 6,
-    maxAge: 8,
-
+    minAge: 6,maxAge: 8,
     title: "Der Schulkiosk",
-
     text: "Im Schulkiosk werden Getränke verkauft.",
-
     question: "4 + 2 = ?",
-
-    answers: [
-      "7",
-      "6",
-      "8"
-    ],
-
+    answers: ["7","6","8"],
     correctAnswer: "6"
   }
 ]
