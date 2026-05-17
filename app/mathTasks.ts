82cca256cd929d9ef13f15063d10f6133c2469fa
Solak-App <serdarsolak2023@gmail.com>
2026-05-17 10:34:36 +0200

    update mathtask.ts and page.tsx

--- app/mathTasks.ts
+++ app/mathTasks.ts
@@ -1,9 +1,20 @@
 export const mathTasks = [
   {
     category: "leicht",
-    minAge: 6,maxAge: 8,
+    minAge: 6,
+    maxAge: 8,
+
+    title: "Plus rechnen",
+    text: "Rechne die Aufgabe nach Ablauf der Zeit.",
+
     question: "4 + 2 = ?",
-    answers: ["7","6","8"],
+
+    answers: [
+      "7",
+      "6",
+      "8"
+    ],
+
     correctAnswer: "6"
   }
-]
+];
