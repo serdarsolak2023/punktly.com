27dac5492c30f0f08122da30f4898fc428602e65
Solak-App <serdarsolak2023@gmail.com>
2026-06-12 19:44:36 +0200

    Add Firestore offline cache support

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+
+import {
+  initializeFirestore,
+  persistentLocalCache,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSmGHj8zZH7kVwV7AdxaqsLvwndbPxkGg",
@@ -12,5 +16,9 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache(),
+});
